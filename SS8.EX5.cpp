8f75aa78c2903dd6ec4cf767c9cf8a9407c53690
domanh09012006 <doducmanh09012006@gmail.com>
2024-11-28 23:31:19 +0700

    Add files via upload

--- SS8.EX5.cpp
+++ SS8.EX5.cpp
@@ -16,7 +16,7 @@ int main(){
 	}
 	for(int i=0; i<a; i++){
 		for(int j=0; j<b; j++){
-			if(i==0 || i == a-1 || j==0 || j ==b-1){
+			if(i==0 || i == a-1 || j==0 || j = =b-1){
 			
 			sum+=array[i][j];
 			}
